97c91d326f4fa1386b6add54e04ec2e9e94b3287
Bigyan Sapkota <bigyansapkotacodes@gmail.com>
2026-03-11 13:24:41 +0545

    add login and register hooks

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
-const client = new QueryClient();
+import { queryClient } from "./lib/query-client.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryClientProvider client={client}>
+    <QueryClientProvider client={queryClient}>
       <App />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
